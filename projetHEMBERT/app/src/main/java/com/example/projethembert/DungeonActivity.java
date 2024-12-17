459d332079d310e12c5c5d13028e631c84d3d5d9
ben-ht <benjamin.hembert2@gmail.com>
2024-12-17 11:59:36 +0100

    projet views

--- projetHEMBERT/app/src/main/java/com/example/projethembert/DungeonActivity.java
+++ projetHEMBERT/app/src/main/java/com/example/projethembert/DungeonActivity.java
@@ -1,9 +1,6 @@
 package com.example.projethembert;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,31 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class DungeonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dungeon);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button dungeon1 = findViewById(R.id.dungeon_1);
-        dungeon1.setOnClickListener(openFightActivity());
-    }
-
-
-    private View.OnClickListener openFightActivity(){
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, DungeonActivity.class);
-                startActivity(intent);
-            }
-        };
     }
 }
